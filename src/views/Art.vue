3f4358267783112ad56f38fe40b350114d299d6e
Ore Landau <ore4444@gmail.com>
2021-03-12 22:15:45 +0200

    lectures

--- src/views/Art.vue
+++ src/views/Art.vue
@@ -22,6 +22,13 @@
             </div>
           </b-tab>
 
+          <b-tab title="הרצאות">
+            <div v-for="lecture in lectures" :key="lecture" class="responsive-google-slides mb-3">
+              <b-embed type="iframe" allowfullscreen
+                       :src="lecture"></b-embed>
+            </div>
+          </b-tab>
+
           <b-tab title="סכמות" active>
             <ImageGallery :images="schemes"></ImageGallery>
           </b-tab>
@@ -49,7 +56,27 @@ export default {
     },
     videos() {
       return ['UztiH2FJHjQ','mR8DAB3YlzM','NMzZZwJoLJo','Q2HSjAX_YiI'].map(_ => 'https://www.youtube.com/embed/' + _)
-    }
+    },
+    lectures() {
+      return ['2PACX-1vQGwSeEcGePtDoX3M5tYWKP5aOReKzdtun6ENazvFS5o5XtzqaOWzeDEoTQNVQeKTsrm8ukyTCNAuyp', '2PACX-1vTQeZJi8owYzDzB0a1D3zgtfVvBhjhhysmNw0gUmC-SCOQ4YbLf2cmqpJAOEnc0W4PMze2ypGxhyH-7', '2PACX-1vRYfyhyMnoRsWhogJoJwjwsbmyCipl8o9HGNYAv-qAo64Kk_Dak8puhWk7IkaKAaTVGE4_4rHIqHx0N'].map(_ => 'https://docs.google.com/presentation/d/e/' + _ + '/embed')
+    },
   }
 }
 </script>
+
+<style lang="scss">
+  .responsive-google-slides {
+    position: relative;
+    padding-bottom: 56.25%; /* 16:9 Ratio */
+    height: 0;
+    overflow: hidden;
+  }
+  .responsive-google-slides iframe {
+    border: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100% !important;
+    height: 100% !important;
+  }
+</style>
